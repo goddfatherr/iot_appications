ec53d7160093c3d0fcab6d9c17592b7ddc8c26eb
asogwa emmanuel <asogwaemmanuel36@gmail.com>
2023-03-11 11:20:38 -0700

    init device status

---
+++ boltiot_device_status/device_restart.py
@@ -0,0 +1,6 @@
+from boltiot import Bolt
+api_key = "451760fb-c462-4b96-b0c2-34e08392543d"
+device_id  = "BOLT15316359"
+mybolt = Bolt(api_key, device_id)
+response = mybolt.restart()
+print (response)
